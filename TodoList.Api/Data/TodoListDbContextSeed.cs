9647d6d41b564aa9c65f11938cc87d384e226597
Bach Ngoc Toan <ngoctoan@live.com>
2021-05-28 15:06:35 +0700

    11. Seeding database

---
+++ TodoList.Api/Data/TodoListDbContextSeed.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using TodoList.Api.Entities;
+using TodoList.Api.Enums;
+using Task = System.Threading.Tasks.Task;
+
+namespace TodoList.Api.Data
+{
+    public class TodoListDbContextSeed
+    {
+        private readonly IPasswordHasher<User> _passwordHasher = new PasswordHasher<User>();
+
+        public async Task SeedAsync(TodoListDbContext context, ILogger<TodoListDbContextSeed> logger)
+        {
+            if (!context.Users.Any())
+            {
+                var user = new User()
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "Mr",
+                    LastName = "A",
+                    Email = "admin1@gmail.com",
+                    PhoneNumber = "032132131",
+                    UserName = "admin"
+                };
+                user.PasswordHash = _passwordHasher.HashPassword(user, "Admin@123$");
+                context.Users.Add(user);
+            }
+
+            if (!context.Tasks.Any())
+            {
+                context.Tasks.Add(new Entities.Task()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Same tasks 1",
+                    CreatedDate = DateTime.Now,
+                    Priority = Priority.High,
+                    Status = Status.Open
+                });
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
--- TodoList.Api/Program.cs
+++ TodoList.Api/Program.cs
@@ -2,10 +2,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using TodoList.Api.Data;
+using TodoList.Api.Extensions;
 
 namespace TodoList.Api
 {
@@ -13,7 +12,14 @@ namespace TodoList.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            host.MigrateDbContext<TodoListDbContext>((context, services) =>
+            {
+                var logger = services.GetService<ILogger<TodoListDbContextSeed>>();
+                new TodoListDbContextSeed().SeedAsync(context, logger).Wait();
+            });
+            host.Run();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +28,8 @@ namespace TodoList.Api
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+
+       
     }
 }
