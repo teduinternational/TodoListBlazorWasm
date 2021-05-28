50f0a7c42bedc672d4f8180174361d996ee4fdfa
Bach Ngoc Toan <ngoctoan@live.com>
2021-05-28 16:35:17 +0700

    13. Implement CRUD APIs

--- TodoList.Api/Controllers/TasksController.cs
+++ TodoList.Api/Controllers/TasksController.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using TodoList.Api.Repositories;
 
@@ -19,12 +16,53 @@ namespace TodoList.Api.Controllers
             _taskRepository = taskRepository;
         }
 
+        //api/tasks
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var tasks =  await _taskRepository.GetTaskList();
+            var tasks = await _taskRepository.GetTaskList();
             return Ok(tasks);
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> Create(Entities.Task task)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var tasks = await _taskRepository.Create(task);
+            return CreatedAtAction(nameof(GetById), new { id = task.Id }, tasks);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update(Guid id, Entities.Task task)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var taskFromDb = await _taskRepository.GetById(id);
+
+            if (taskFromDb == null)
+            {
+                return NotFound($"{id} is not found");
+            }
+
+            taskFromDb.Name = task.Name;
+            var tasks = await _taskRepository.Update(task);
+
+            return Ok(tasks);
+        }
+
+
+        //api/tasks/xxxx
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            var task = await _taskRepository.GetById(id);
+            if (task == null) return NotFound($"{id} is not found");
+            return Ok(task);
         }
     }
 }
